3624ad580162ee2e76245e6e870da1aeb19c0d57
Callum Leahy <callumleahy23@googlemail.com>
2016-10-17 10:24:27 +0100

    set up index.js and package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var ejs = require('ejs');
+var layouts= require('express-ejs-layouts');
+// requiring the routes files
+var routes = require('./config/routes');
+//constructing express
+var app = express();
+//sets the view engine for use with res.render
+app.set('view engine', 'ejs');
+//uses the layouts modules
+app.use(layouts);
+// uses the express module
+app.use(routes);
+
+//routes go between requirements and listen
+app.listen(3000 , function(){
+  console.log('app is listening on port 3000');
+});
